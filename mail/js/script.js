5564312701e1c4c59073f3fe9c2e596f628140dd
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-26 15:52:15 +0200

    creazione array + richiesta all'utente

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,9 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (che hai fatto tu precedentemente), stampa un messaggio appropriato sull’esito del controllo.
+
+//  creazione array
+const pcMail = ["passarotto13@gmail.com", "tpassarotto@gmail.com", "tommaso.passarotto@gmail.com"];
+// console.log(pcMail);
+
+// Richiesta all'utente
+const userMail = prompt("Scrivi qui la tua Email: ");
+console.log(userMail);
